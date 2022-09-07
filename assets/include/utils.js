8033c3edc44207ae73f1ec2a6579debc9cac50b2
kotnid <tkt0506tkt@gmail.com>
2022-09-07 22:04:18 +0800

    smsccw : Big change - remove pad view - use vmin for font size - remove repeated css - fix image view - responsive website - remove link - center link - fix scrolling problem (by remove skrollr)
    Signed-off-by: kotnid <tkt0506tkt@gmail.com>

--- assets/include/utils.js
+++ assets/include/utils.js
@@ -1,11 +1,14 @@
 /* util file for smsccw */
 /* version 1.0 */
 
-function hideAllCards() {
+function hideAllCards(ppl = 'none') {
     var cards = document.querySelectorAll(".namecard");
      /* console.log(cards); */
     for (var i = 0, element; element = cards[i]; i++) {
-        console.log(element);
+        console.log(element.classList[1]);
+        if (element.classList[1] == ppl+'-card'){
+            continue;
+        }
         element.style.display = 'none';
     }
 }
@@ -13,43 +16,67 @@ function hideAllCards() {
 
 function card_onclick(name){
     console.log(name+" is clicked!");
-    hideAllCards();
+    hideAllCards(name);
 
     switch(name){
         case "donald":
             var ele = document.getElementsByClassName('donald-card')[0];
-            console.log(ele);
-            ele.style.display = '';
+            console.log(ele.style.display);
+            if (ele.style.display == 'none'){
+                ele.style.display = '';
+            }else{
+                ele.style.display = 'none';
+            }
             break;
         
         case "jy":
             var ele = document.getElementsByClassName('jy-card')[0];
             console.log(ele);
-            ele.style.display = '';
+            if (ele.style.display == 'none'){
+                ele.style.display = '';
+            }else{
+                ele.style.display = 'none';
+            }
             break;
 
         case "tkt":
             var ele = document.getElementsByClassName('tkt-card')[0];
             console.log(ele);
-            ele.style.display = '';
+            if (ele.style.display == 'none'){
+                ele.style.display = '';
+            }else{
+                ele.style.display = 'none';
+            }
             break;
 
         case "pig":
             var ele = document.getElementsByClassName('pig-card')[0];
             console.log(ele);
-            ele.style.display = '';
+            if (ele.style.display == 'none'){
+                ele.style.display = '';
+            }else{
+                ele.style.display = 'none';
+            }
             break;
 
         case "left":
             var ele = document.getElementsByClassName('left-card')[0];
             console.log(ele);
-            ele.style.display = '';
+            if (ele.style.display == 'none'){
+                ele.style.display = '';
+            }else{
+                ele.style.display = 'none';
+            }
             break;
         
         case "jamie":
             var ele = document.getElementsByClassName('jamie-card')[0];
             console.log(ele);
-            ele.style.display = '';
+            if (ele.style.display == 'none'){
+                ele.style.display = '';
+            }else{
+                ele.style.display = 'none';
+            }
             break;
     }
 }
@@ -94,7 +121,7 @@ const observer = new IntersectionObserver(entries => {
 });
   
 observer.observe(document.querySelector('.up-wrapper'));
-observer.observe(document.querySelector('.up-wrapper1'));
+//observer.observe(document.querySelector('.up-wrapper1'));
 observer.observe(document.querySelector('.up-wrapper2'));
 observer.observe(document.querySelector('.down-wrapper'));
 observer.observe(document.querySelector('.down-wrapper1'));

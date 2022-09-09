81411c6e1c07f13844b0110eb7e6dc6a1d61e31a
kotnid <tkt0506tkt@gmail.com>
2022-09-09 21:55:01 +0800

    smsccw : Edit animation + blod date + remove donald
    Signed-off-by: kotnid <tkt0506tkt@gmail.com>

--- assets/include/utils.js
+++ assets/include/utils.js
@@ -41,7 +41,30 @@ if (screen.width < 613){
     });
 }
 
+const getOffset = (el) => {
+    const rect = el.getBoundingClientRect();
+    return {
+      left: rect.left + window.pageXOffset,
+      top: rect.top + window.pageYOffset,
+      width: rect.width || el.offsetWidth,
+      height: rect.height || el.offsetHeight
+    };
+}
 
+function connect(name){
+    var ele1 = document.getElementsByClassName(name+'-card')[0];
+    var ele2 = document.getElementsByClassName(name)[0];
+    var line = document.getElementById("line");
+
+    const off1 = getOffset(ele1);
+    const off2 = getOffset(ele2);
+
+    const x1 = off1.left + off1.width;
+    const y1 = off1.top + off1.height;
+
+    const x2 = off2.left + off2.width;
+    const y2 = off2.top;
+}
 
 const observer = new IntersectionObserver(entries => {
     entries.forEach(entry => {
@@ -70,4 +93,4 @@ observer.observe(document.querySelector('.up-wrapper'));
 //observer.observe(document.querySelector('.up-wrapper1'));
 observer.observe(document.querySelector('.up-wrapper2'));
 observer.observe(document.querySelector('.down-wrapper'));
-observer.observe(document.querySelector('.down-wrapper1'));
+observer.observe(document.querySelector('.down-wrapper2'));
